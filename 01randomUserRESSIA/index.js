6209c3680c3452c7a13930dc9c47296d1f2e5f1a
ressiasofia <s.ressia.3954@vallauri.edu>
2025-12-24 15:25:40 +0100

    commit

--- 01randomUserRESSIA/index.js
+++ 01randomUserRESSIA/index.js
@@ -12,25 +12,56 @@ const lblSlider = document.getElementsByTagName("label")[0];
 let nUsers = 0;
 
 let params = {
-    results:"50"
+    results: "50"
 }
 
-slider.addEventListener("change",function(){
+slider.addEventListener("change", function () {
     nUsers = parseInt(slider.value);
     lblSlider.innerHTML = `Users to view: ${nUsers}`;
+
+    params.results = nUsers;
 })
 
 //functions
 loadCard(params);
 
-function loadCard(params){
-    let promise = ajax.sendRequest("GET","./api",params);
+function loadCard(params) {
+    let promise = ajax.sendRequest("GET", "./api", params);
 
     promise.catch(ajax.errore);
-    promise.then(function(httpResponse){
+    promise.then(function (httpResponse) {
         let people = httpResponse.data.results;
         console.log(people)
+
+        for (let person of people) {
+            let card = document.createElement("div");
+            card.classList.add("card");
+            cardBody.append(card);
+
+            let cardInner = document.createElement("div");
+            cardInner.classList.add("cardInner");
+            cardInner.addEventListener("click",function(){
+                $(this).toggleClass("flipped");
+            })
+            card.append(cardInner);
+
+            let cardFront = document.createElement("div");
+            cardFront.classList.add("cardFront");
+
+            let img = document.createElement("img");
+            img.src = `${person.picture.medium}`;
+            cardFront.append(img);
+
+            let Name = document.createElement("div");
+            Name.textContent = `${person.name.first} ${person.name.last}`;
+            cardFront.append(Name);
+
+            let cardBack = document.createElement("div");
+            cardBack.classList.add("cardBack");
+
+
+            cardInner.append(cardFront, cardBack);
+        }
     })
-
-
 }
+
